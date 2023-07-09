1b34304b5ff540e92892af74073b35a5939cdb04
Mona Lisa <mona@lisa.vinci>
2023-07-09 15:34:41 +0200

    Add basic nav

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,21 @@
+import Navbar from "./Navbar";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import Home from "./Home";
+import Players from "./Players";
+import Teams from "./Teams";
+
 export default function App () {
   return (
-    <div>
-      Hash History Basketball League
-    </div>
+    <BrowserRouter>
+      <div>
+        <Navbar/>
+
+        <Routes>
+          <Route path={'/'} element={<Home/>}/>
+          <Route path={'/players'} element={<Players/>}/>
+          <Route path={'/teams'} element={<Teams/>}/>
+        </Routes>
+      </div>
+    </BrowserRouter>
   )
 }
